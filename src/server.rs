15961527e6a53350d5d53ccd68d98eaf3269d11b
aaronhtan <aaronhtan@gmail.com>
2025-08-21 16:46:46 +0100

    working on server implementation. beginning with basic system format, and organizing the main program loop.

--- src/server.rs
+++ src/server.rs
@@ -5,27 +5,65 @@
 /// specified readDrops or if used with a specific connection, an error is given there. There is
 /// a specific handler for each client function.
 #[allow(unused_imports)]
-use std::sync::mpsc;
+use std::{sync::mpsc, thread};
+use tokio::{self, sync::mpsc as tmpsc};
+
+use tokio_stream::{self as stream, StreamExt};
 
 struct Message;
 #[allow(dead_code)]
-struct ClientConn {
-    
-    receiver: mpsc::Receiver<Message>,
 
+struct ClientConn {
+    receiver: mpsc::Receiver<Message>,
 }
 
-struct Server {}
+pub struct Server {
+    read_channel: 
+}
 
 #[allow(dead_code)]
 impl Server {
-    fn new() {}
+    pub fn new() -> Server {
+        let (work_tx, mut work_rx) = tmpsc::channel::<()>(1);
+        Server {}
+    }
 
-    fn read() {}
+    /// serve is the main loop that handles various events atomically, preventing any dataraces.
+    /// It interacts with numerous other threads, including ones that store various client
+    /// interaction data, the main listening loop for handling new connections, and also
+    /// interacting with client functions as well.
+    ///
+    async fn serve(& self) {
+        let mut stream1 = stream::iter(vec![1, 2, 3]);
+        let mut stream2 = stream::iter(vec![4, 5, 6]);
+        let mut values = vec![];
+        loop {
+            tokio::select! {
+                Some(v) = stream1.next() => values.push(v),
+                Some(v) = stream2.next() => values.push(v),
+                else => break,
+            }
+        }
+    }
+    pub fn read() {}
 
-    fn write() {}
+    pub fn write() {}
 
-    fn read_id() {}
+    pub fn read_id() {}
 
-    fn write_id() {}
+    pub fn write_id() {}
+
+    pub fn is_valid() {}
+
+    pub fn client_list() {}
+
+    pub fn close() {}
+}
+
+fn read_to_client(client_id: u32) {
+
+}
+
+fn write_to_client() {
+
 }
