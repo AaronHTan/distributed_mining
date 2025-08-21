4703f8551f3025dfa7521593800e89ee941685fc
aaronhtan <aaronhtan@gmail.com>
2025-08-21 19:17:23 +0100

    Working on server implementation. Decided upon implementation where there is a dedicated server loop with its own resources and that runs async, and communicates via channels. There is also a listener loop, that is just a tokio task, as this one mainly blocks in IO and is not CPU intensive. Furthermore, we have a server object that returns to the original thread, and this one is how you can interact with the server to complete various tasks, such as reading, writing to all and specific client connections. Hesistant on whether to keep async - if remove async, must then also change the channels to standard.

--- src/server.rs
+++ src/server.rs
@@ -5,65 +5,143 @@
 /// specified readDrops or if used with a specific connection, an error is given there. There is
 /// a specific handler for each client function.
 #[allow(unused_imports)]
-use std::{sync::mpsc, thread};
-use tokio::{self, sync::mpsc as tmpsc};
+use std::{error::Error, sync::mpsc, thread};
+use tokio::{self, runtime::Builder, sync::broadcast, sync::mpsc as tmpsc};
 
-use tokio_stream::{self as stream, StreamExt};
+/// TODO: Finish implementing the Server structure
+/// Publicly exposed server struct that represents a server instance.
+pub struct Server {
+    status: ServerStatus,
+    close_tx: broadcast::Sender<()>,
+    read_client_tx: tmpsc::Sender<Message>,
+}
 
+/// Status of the server
+enum ServerStatus {
+    Running,
+    NotStarted,
+    NotRunning,
+    Closed,
+}
+/// Message structure for interfacing with various reads, writes to and from the clients
+/// TODO: Implement a generic message type that the server can handle, hopefully at runtime
 struct Message;
 #[allow(dead_code)]
 
+///
+///
+///
 struct ClientConn {
-    receiver: mpsc::Receiver<Message>,
+    receiver: tmpsc::Receiver<Message>,
 }
 
-pub struct Server {
-    read_channel: 
+struct ListenerState {}
+
+/// TODO: Finish defining the server state
+///
+struct ServerState {
+    close_rx: broadcast::Receiver<()>,
+    read_client_rx: tmpsc::Receiver<Message>,
 }
 
+/// TODO:Since creating a server spawns resources and  threads that we need to clean up
+/// afterwards, If the server is still running, we must clean up the resources.
+impl Drop for Server {
+    fn drop(&mut self) {}
+}
 #[allow(dead_code)]
 impl Server {
-    pub fn new() -> Server {
-        let (work_tx, mut work_rx) = tmpsc::channel::<()>(1);
-        Server {}
+    /// Creates new server instance.
+    pub fn new() -> Result<Server, Box<dyn Error>> {
+        let (read_client_tx, mut read_client_rx) = tmpsc::channel(1);
+        let (close_tx, close_rx) = broadcast::channel(1);
+        let server_state = ServerState {
+            close_rx: close_rx,
+            read_client_rx: read_client_rx,
+        };
+
+        // NOTE: New thread entrypoint: runs the server separately to maximize
+        // server efficiency
+        std::thread::spawn(move || {
+            let Ok(rt) = Builder::new_multi_thread()
+                .worker_threads(2) // NOTE: Change as required
+                .build()
+            else {
+                return;
+            };
+            rt.block_on(server_state.serve());
+        });
+
+        Ok(Server {
+            status: ServerStatus::NotStarted,
+            close_tx: close_tx,
+            read_client_tx: read_client_tx,
+        })
     }
 
-    /// serve is the main loop that handles various events atomically, preventing any dataraces.
-    /// It interacts with numerous other threads, including ones that store various client
-    /// interaction data, the main listening loop for handling new connections, and also
-    /// interacting with client functions as well.
-    ///
-    async fn serve(& self) {
-        let mut stream1 = stream::iter(vec![1, 2, 3]);
-        let mut stream2 = stream::iter(vec![4, 5, 6]);
-        let mut values = vec![];
+    pub fn start(&mut self) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+
+    pub fn read(&self) -> Result<Message, Box<dyn Error>> {
+        Ok(Message {})
+    }
+
+    pub fn write(&self, message: Message) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+
+    pub fn read_close(&self) -> Result<Vec<u32>, Box<dyn Error>> {
+        Ok(vec![])
+    }
+    pub async fn read_id(&self, conn_id: u32) -> Result<Message, Box<dyn Error>> {
+        Ok(Message {})
+    }
+
+    pub async fn write_id(&self, conn_id: u32, message: Message) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+
+    pub fn is_valid(&self, conn_id: u32) -> Result<bool, Box<dyn Error>> {
+        Ok(false)
+    }
+
+    pub fn client_list(&self) -> Result<Vec<u32>, Box<dyn Error>> {
+        Ok(vec![])
+    }
+
+    pub fn close(&self) -> Result<usize, Box<dyn Error>> {
+        let k = self.close_tx.send(())?;
+        Ok(k)
+    }
+}
+
+/// TODO: Finish implementing the server state
+/// serve is the main loop that handles various events atomically, preventing any dataraces.
+/// It interacts with numerous other threads, including ones that store various client
+/// interaction data, the main listening loop for handling new connections, and also
+/// interacting with client functions as well. Only Inner is capable of accessing the
+/// inner server as well.
+impl ServerState {
+    async fn serve(mut self) -> Result<(), Box<dyn Error + Send>> {
         loop {
             tokio::select! {
-                Some(v) = stream1.next() => values.push(v),
-                Some(v) = stream2.next() => values.push(v),
-                else => break,
+                value = self.read_client_rx.recv() => (),
+                _ = self.close_rx.recv() => return Ok(()),
+
             }
         }
     }
-    pub fn read() {}
-
-    pub fn write() {}
-
-    pub fn read_id() {}
-
-    pub fn write_id() {}
-
-    pub fn is_valid() {}
-
-    pub fn client_list() {}
-
-    pub fn close() {}
 }
 
-fn read_to_client(client_id: u32) {
-
+/// TODO: Finish implementing the listener state.
+impl ListenerState {
+    async fn listen(mut self) {}
 }
 
-fn write_to_client() {
+fn read_to_client(client_id: u32) {}
 
-}
+fn write_to_client() {}
